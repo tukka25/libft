68c2554fe1275acd89db68aa406b80d1d8bd77e5
tukka <aboodytukka@gmail.com>
2022-10-02 16:52:20 +0400

    tukka

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: abdamoha <abdamoha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 17:09:54 by abdamoha          #+#    #+#             */
-/*   Updated: 2022/10/02 16:29:20 by abdamoha         ###   ########.fr       */
+/*   Updated: 2022/10/02 16:50:56 by abdamoha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,20 @@ char	*ft_strdup(const char *s1)
 {
 	char	*str;
 	int		i;
-	int		n;
 
 	i = 0;
-	str = (char *)malloc((sizeof(char) + 1));
-	str = (char *)s1;
-	while (str[i] != '\0')
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	str = (char *)malloc((i * sizeof(char)) + 1);
+	i = 0;
+	while (s1[i] != '\0')
 	{
 		str[i] = s1[i];
 		i++;
 	}
+	str[i] = '\0';
 	return (str);
 }
 //  int main()
